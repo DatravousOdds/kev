3e46237de708a8395d1e87346786fb27335e578e
Travis Odds <datravousodds@gmail.com>
2023-12-27 17:34:45 +0000

    nav function created

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 import lxml
+import json
+
 
 testUrl = "https://www.nike.com/"
 
@@ -13,9 +16,18 @@ chrome_options.add_argument("--disable-dev-shm-usage")
 driver = webdriver.Chrome(options=chrome_options)
 driver.get(testUrl)
 
+
 # TODO: Need to build a function that will navigate to product page.
+def naviagteToProductPage(url):
+  try:
+    driver.get(url)
+  except Exception as e:
+    print(f'Error occur when navigatin to the page: {e}')
+
 
 # TODO: Filter out shoes that don't match the preferences.
+
+# TODO: Build a function that select item base off user preferences.
 
 
 class Bot:
@@ -33,12 +45,11 @@ class Bot:
       soup = BeautifulSoup(r.text, 'lxml')
 
       product_name = soup.find_all('div', class_="product-card__title")
-      
+
       product_price = soup.find_all(
           "div",
           class_="product-price us__styling is--current-price css-11s12ax")
       product_details = zip(product_name, product_price)
-      # print(product_details)
 
       # Printing product details
       print("{:<40} | {:<10}".format("Product Name", "Product Price"))
@@ -46,7 +57,7 @@ class Bot:
 
       for name, price in product_details:
         print("{:<40} | {:<10}".format(name.text.strip(), price.text.strip()))
-      # print(product_details)
+
       return product_details  # Return poduct details for further processing
 
     except requests.RequestException as e:
@@ -62,6 +73,7 @@ class User:
   def __init__(self, name, email):
     self.name = name
     self.email = email
+    self.bot = Bot()
 
   def greet(self):
     print(f"Welcome, {self.name} What are we doing today?\n")
@@ -75,19 +87,14 @@ class User:
       self.greet()
       choice = input("Choose your option:\n")
       if choice == "1":
-        url = testUrl  # Gets url to track
-        driver.get(url)
-        categoryChoice = driver.find_element_by_xpath('//*[@id="gen-nav-commerce-header-v2"]/div[3]/header/div[1]/div[2]/nav/div[2]/div/ul/li[2]/a')
-        categoryChoice.click()
-        menCategory = driver.find_element_by_xpath('//*[@id="08fa77c7-66ea-4345-a592-cb13c26e1a34"]/div/section/ul/li[2]/figure/a')
-        menCategory.click()
+        url = testUrl  # Gets the URL input from the user
+        self.bot.urls(url)
+        naviagteToProductPage(url)
       elif choice == "2":
         urls = input("Enter url (separated by space, ):")
-        # print(urls)T
         urls = urls.split(" ")
         for url in urls:
           print(url)
-          # product_details = processData(url)
       elif choice == "3":
         print("Dash board")
       elif choice == "4":
@@ -98,8 +105,8 @@ class User:
     print("What are your preferences?")
     gender = input("Gender: ")
     shoeSize = input("Shoe size: ")
-    # print(gender)
-    # print(shoeSize)
+    shoeName = input("Shoe name: ")
+    return gender, shoeSize, shoeName
 
 
 name = input("Please enter your name:\n")
